534ad030f4ec393f10f1901d5078dd99f4a28a16
Yucsan <yucsan2023@gmail.com>
2025-01-23 20:25:12 +0100

    agrego tarea paises sin terminar

---
+++ PSP_SPRING/ApiRest_ZPaises/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into pais (nombre) values ('Peŕu');
+insert into pais (nombre) values ('España');
+insert into pais (nombre) values ('Usa');
+insert into pais (nombre) values ('Colombia');
+insert into pais (nombre) values ('Francia');
