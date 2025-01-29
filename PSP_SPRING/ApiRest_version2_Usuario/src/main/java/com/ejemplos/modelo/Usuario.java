81185b369617e238d3cc5fbbf5926a0ded6c79c3
Yucsan <yucsan2023@gmail.com>
2025-01-29 12:08:01 +0100

    APIRestV2_usuario correct

--- PSP_SPRING/ApiRest_version2_Usuario/src/main/java/com/ejemplos/modelo/Usuario.java
+++ PSP_SPRING/ApiRest_version2_Usuario/src/main/java/com/ejemplos/modelo/Usuario.java
@@ -1,53 +1,23 @@
 package com.ejemplos.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
 public class Usuario {
 	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String nombre;
 	private String avatar;
 	private String mail;
-	
-	public Usuario(long id, String nombre, String avatar, String mail) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.avatar = avatar;
-		this.mail = mail;
-	}
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-	
-	
 	
 	
 
