dc368819dc00175471fdfe398084705462fc8f68
Yucsan <yucsan2023@gmail.com>
2025-01-30 10:25:50 +0100

    avance Jueves

--- PSP_SPRING/ApiRest_version2_Usuario/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into USUARIO (id, nombre, avatar, mail) values (NEXTVAL('USUARIO_SEQ'), 'Fernando','hero' 'avatar1','core@gmail.com');
-insert into USUARIO (id, nombre, avatar, mail) values (NEXTVAL('USUARIO_SEQ'), 'Rita','hero2' 'avatar3','core2@gmail.com');
-insert into USUARIO (id, nombre, avatar, mail) values (NEXTVAL('USUARIO_SEQ'), 'josefina','hero3' 'avatar3','core3@gmail.com');
-
-
---
+++ PSP_SPRING/apiWEB/src/main/java/com/ejemplos/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.ejemplos.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ejemplos.modelo.Usuario;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+	@Value("${rutaapi}")
+	String basePath;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public List<Usuario>ObtenerUsuarios(){
+		Usuario[] response = restTemplate.getForObject(basePath+"usuario", Usuario[].class);
+				return Arrays.asList(response);
+	}
+	
+	
+}
